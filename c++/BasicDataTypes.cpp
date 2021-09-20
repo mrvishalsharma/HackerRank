0dbd6710df6c6e3a5dc8fc322adc440a13ec3ff2
mrvishalsharma <mrvishalsharma8@gmail.com>
2021-09-20 20:25:54 +0530

    solved Basic Data Type Problem

---
+++ c++/BasicDataTypes.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    long n2;
+    char a;
+    float f;
+    double d;
+    
+    cin>>n>>n2>>a>>f>>d;
+    
+    cout<<n<<endl;
+    cout<<n2<<endl;
+    cout<<a<<endl;
+    cout<<fixed<<setprecision(3)<<f<<endl;
+    cout<<fixed<<setprecision(9)<<d<<endl;
+    
+    return 0;
+}
